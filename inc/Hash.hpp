fd93b1ad46b65be433ea9a5d06f786e5f3019b27
Evan Wilde <etcwilde@uvic.ca>
2015-06-17 10:11:20 -0700

    Raw Hash
    For transporting the hash contents, we don't want the string, we want to copy
    the contents of the payload directly into the hash buffer.
    
    The constructor takes a string and optionally a boolean. We assume that the
    input string is not hashed and will hash it unless the boolean is set, in which
    case we copy the contents of the string into the hash buffer.
    
    We also output the raw hash buffer using the raw() method.

--- inc/Hash.hpp
+++ inc/Hash.hpp
@@ -14,10 +14,13 @@
 #include <sstream>
 
 #include <string.h>
+#include <stdio.h>
 #include <openssl/sha.h>
 
-#define HASH_LENGTH_BITS 160
+//#define HASH_LENGTH_BITS 160
+//#define HASH_LENGTH_BYTES 20
 #define HASH_LENGTH_BYTES 20
+#define HASH_LENGTH_BITS HASH_LENGTH_BYTES * 8
 
 /**
  * Hash
@@ -26,88 +29,97 @@
  */
 class Hash
 {
-public:
-	Hash() { memset(m_hash, 0, HASH_LENGTH_BYTES); }
+	public:
+		Hash() { memset(m_hash, 0, HASH_LENGTH_BYTES); }
 
-	Hash(const std::string& message)
-	{
-		memset(m_hash, 0, HASH_LENGTH_BYTES);
-		SHA1((const unsigned char*)message.c_str(), message.size(), m_hash);
-	}
-
-
-	inline void operator=(const Hash& other)
-	{ memcpy(m_hash, other.m_hash, HASH_LENGTH_BYTES); }
-
-
-	int compareTo(const Hash& other) const
-	{
-		for (unsigned int i = 0; i < HASH_LENGTH_BYTES; i++)
+		Hash(const std::string& message, bool hashed=false)
 		{
-			const int loperand = m_hash[i] & 0xff;
-			const int roperand = other.m_hash[i] & 0xff;
-			if (loperand != roperand) return (loperand - roperand);
+			if (!hashed)
+			{
+				memset(m_hash, 0, HASH_LENGTH_BYTES);
+				SHA1((const unsigned char*)message.c_str(), message.size(), m_hash);
+			}
+			else memcpy(m_hash, message.c_str(), HASH_LENGTH_BYTES);
 		}
-		return 0;
-	}
 
-	bool between(const Hash& smaller, const Hash& larger)
-	{
-		int small_large = smaller.compareTo(larger);
-		if (small_large < 0)
+
+
+		inline void operator=(const Hash& other)
+		{ memcpy(m_hash, other.m_hash, HASH_LENGTH_BYTES); }
+
+
+		int compareTo(const Hash& other) const
 		{
-			if (compareTo(smaller) > 0 && compareTo(larger) < 0)
-				return true;
+			for (unsigned int i = 0; i < HASH_LENGTH_BYTES; i++)
+			{
+				const int loperand = m_hash[i] & 0xff;
+				const int roperand = other.m_hash[i] & 0xff;
+				if (loperand != roperand) return (loperand - roperand);
+			}
+			return 0;
 		}
-		else if (small_large > 0) // small and large are reversed
+
+		bool between(const Hash& smaller, const Hash& larger)
 		{
-			if (compareTo(larger) < 0 || compareTo(smaller) > 0)
-				return true;
+			int small_large = smaller.compareTo(larger);
+			if (small_large < 0)
+			{
+				if (compareTo(smaller) > 0 && compareTo(larger) < 0)
+					return true;
+			}
+			else if (small_large > 0) // small and large are reversed
+			{
+				if (compareTo(larger) < 0 || compareTo(smaller) > 0)
+					return true;
+			}
+			return false;
 		}
-		return false;
-	}
 
-	inline bool operator<(const Hash& other) const
-	{ return compareTo(other) < 0; }
+		inline bool operator<(const Hash& other) const
+		{ return compareTo(other) < 0; }
 
-	inline bool operator>(const Hash& other) const
-	{ return compareTo(other) > 0; }
+		inline bool operator>(const Hash& other) const
+		{ return compareTo(other) > 0; }
 
-	inline bool operator==(const Hash& other) const
-	{ return compareTo(other) == 0; }
+		inline bool operator==(const Hash& other) const
+		{ return compareTo(other) == 0; }
 
-	inline bool operator<= (const Hash& other) const
-	{ return compareTo(other) <= 0; }
+		inline bool operator<= (const Hash& other) const
+		{ return compareTo(other) <= 0; }
 
-	inline bool operator>= (const Hash& other) const
-	{ return compareTo(other) >= 0; }
+		inline bool operator>= (const Hash& other) const
+		{ return compareTo(other) >= 0; }
 
 
-	inline const unsigned char operator[](unsigned int i) const { return m_hash[i]; }
-	inline unsigned long size() const { return HASH_LENGTH_BYTES; }
+		inline const unsigned char operator[](unsigned int i) const { return m_hash[i]; }
+		inline unsigned long size() const { return HASH_LENGTH_BYTES; }
 
-	std::string toString() const
-	{
-		std::stringstream os; os << std::hex;
-		for (unsigned int i = 0; i < HASH_LENGTH_BYTES; i++)
-			os << static_cast<int>(m_hash[i]);
-		return os.str();
-	}
-
-
-	// Hashes the hash for use in hash tables using djb2
-	unsigned long djb2() const
-	{
-		unsigned long hash = 5381;
-		for (unsigned long c = 0; c < HASH_LENGTH_BYTES; ++c)
+		std::string toString() const
 		{
-			hash = ((hash << 5) + hash) + m_hash[c];
+			std::stringstream os; os << std::hex;
+			for (unsigned int i = 0; i < HASH_LENGTH_BYTES; i++)
+				os << static_cast<int>(m_hash[i]);
+			return os.str();
 		}
-		return hash;
-	}
 
-private:
-	unsigned char m_hash[HASH_LENGTH_BYTES];
+		std::string raw() const
+		{
+			return std::string(reinterpret_cast<const char*>(&m_hash[0]),
+					HASH_LENGTH_BYTES);
+		}
+
+
+		// Hashes the hash for use in hash tables using djb2
+		unsigned long djb2() const
+		{
+			unsigned long hash = 5381;
+			for (unsigned long c = 0; c < HASH_LENGTH_BYTES; ++c)
+				hash = ((hash << 5) + hash) + m_hash[c];
+			return hash;
+		}
+
+	private:
+		unsigned char m_hash[HASH_LENGTH_BYTES];
 };
 
 // Nah
